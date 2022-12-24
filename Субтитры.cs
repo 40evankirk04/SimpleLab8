14b89fce787af0549ef1dfbe3eae5af93aa3dec8
Ivan <evankirk@vk.com>
2022-12-24 20:27:24 +0500

    Создание DrawScreen() и Subtitles()

--- Субтитры.cs
+++ Субтитры.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,22 @@ namespace SimpleLab8
 {
     public class Task_1
     {
+        static void DrawScreen()
+        {
+            
+        }
+
+        static void ShowSubtitles()
+        {
+            foreach (string line in File.ReadLines(@"Subtitles.txt"))
+            {
+                
+            }
+        }
+
         public static void Main()
         {
-
+            ShowSubtitles();
         }
     }
 }
