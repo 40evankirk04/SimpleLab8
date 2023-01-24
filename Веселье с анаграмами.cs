6d496c67c4b398bb291fba6dae97e552ae334d57
Ivan <evankirk@vk.com>
2023-01-24 17:20:05 +0500

    Task_2 is completed

--- Веселье с анаграмами.cs
+++ Веселье с анаграмами.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SimpleLab8
+namespace Progaram
 {
-    public class Task_3
+    class Program
     {
+        public static void Main()
+        {
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
 
+            string[] result = CheckAnagrams(array);
+
+            Print(result);
+        }
+
+        public static string[] CheckAnagrams(string[] array)
+        {
+            List<string> result = new List<string>();
+
+            string lastWord = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string word = SortSymbol(array[i]);
+
+                if (word != lastWord)
+                {
+                    result.Add(array[i]);
+
+                    lastWord = word;
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public static string SortSymbol(string word)
+        {
+            char[] charsArray = word.ToCharArray();
+
+            Array.Sort(charsArray);
+
+            return String.Join("", charsArray);
+        }
+
+        public static void Print(string[] array)
+        {
+            string result = "";
+
+            foreach (var elem in array)
+            {
+                result += $"\'{elem}\' ";
+            }
+
+            Console.WriteLine($"[{result.Trim()}]");
+        }
     }
 }
